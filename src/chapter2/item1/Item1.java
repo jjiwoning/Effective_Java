9d9b3a0ab5ff7cfba8f95003908dc6dbd5f68dcb
JJiWoNing <jjiwoning12@gmail.com>
2022-12-06 16:34:51 +0900

    Initial commit

---
+++ src/chapter2/item1/Item1.java
@@ -0,0 +1,30 @@
+package chapter2.item1;
+
+public class Item1 {
+    private int num;
+    private String name;
+
+    //기존의 방식 -> public으로 생성자를 열어서 쓴다.
+    public Item1(int num, String name) {
+        this.num = num;
+        this.name = name;
+    }
+
+    public Item1() {
+    }
+
+    //정적 팩터리 메서드
+    public static Item1 getInstance(int num, String name) {
+        Item1 item1 = new Item1();
+        item1.num = num;
+        item1.name = name;
+        return item1;
+    }
+
+    public static Item1 getInheritance(int num, String name) {
+        Item1 item = new Item1_Inheritance();
+        item.num = num;
+        item.name = name;
+        return item;
+    }
+}
