d2ad24a940d44c4257bc8367f4a17d4b5464efd5
JJiWoNing <jjiwoning12@gmail.com>
2022-12-18 20:14:10 +0900

    Item14. Comparable을 구현할지 고려해라

--- src/main/java/chapter3/item14/Item14Main.java
+++ src/main/java/chapter3/item14/Item14Main.java
@@ -1,10 +1,14 @@
 package chapter3.item14;
 
+import java.util.Arrays;
+
 /**
  * 아이템14. Comparable을 구현할지 고려해라
  */
 public class Item14Main {
     public static void main(String[] args) {
-
+        int[] arr = new int[]{3, 4, 5, 1, 2};
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
     }
 }
